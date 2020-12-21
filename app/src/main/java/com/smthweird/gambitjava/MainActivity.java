b085bb681afc9b60161c44aabfeffe754d12ab63
TaibAkhmedov <taibaxm@gmail.com>
2020-12-21 19:42:39 +0300

    ///

--- app/src/main/java/com/smthweird/gambitjava/MainActivity.java
+++ app/src/main/java/com/smthweird/gambitjava/MainActivity.java
@@ -1,14 +1,69 @@
 package com.smthweird.gambitjava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<FoodItem> foodItemList;
+    private FoodAdapter foodAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initRecyclerView();
+
+        App.getGambitApiApi().getFoodItemList().enqueue(new Callback<List<FoodItem>>() {
+            @Override
+            public void onResponse(Call<List<FoodItem>> call, Response<List<FoodItem>> response) {
+                if (response.isSuccessful()) {
+                    foodItemList = response.body();
+                    foodAdapter = new FoodAdapter(foodItemList);
+                    recyclerView.setAdapter(foodAdapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<FoodItem>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "УПС?", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+
+    }
+
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            Toast.makeText(MainActivity.this, "My first Swipe!", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public void initRecyclerView() {
+        recyclerView = findViewById(R.id.RecyclerView_activity_main);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
